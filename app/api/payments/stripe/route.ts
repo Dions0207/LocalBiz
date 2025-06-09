1c8641a55ca367084166d5739a1cb7f3ecfb274a
Dions0207 <driversdions@gmail.com>
2025-06-09 18:58:36 -0400

    Update route.ts

--- app/api/payments/stripe/route.ts
+++ app/api/payments/stripe/route.ts
@@ -2,7 +2,7 @@ import { type NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil', // Versión actualizada
+  apiVersion: "2025-04-30.basil", // Versión actualizada
 });
 
 export async function POST(request: NextRequest) {
